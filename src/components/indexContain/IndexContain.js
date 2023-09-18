b56bad099f95d3dbdb0d548b72ef97839b67bf79
Ruthline <rutheveline@hotmail.es>
2023-09-17 23:34:31 -0500

    We have started with the content of our home page, adjustments are expected.

---
+++ src/components/indexContain/IndexContain.js
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import '../indexContain/IndexContain.css';
+import PeopleImg from '../indexContain/people-index.png';
+
+
+function IndexContain() {
+    return(
+        <div className='container-index'>
+            <div className='invite-information'>
+                <h2>
+                Nos gusta que te sientas bien y hemos desarrollado una metodología de ayuda para que empieces un cambio saludable
+                </h2>
+                <p>Nuestros planes se basan de la extracción de tus datos con una ficha antropométrica, en donde te asignamos el plan de actividad física y el respectivo plan nutricional que te convenga. Permítenos ser parte de este cambio.</p>
+
+                <div className='start-button-section'>
+                    <a><Link to="/" className='start-button'>Empieza gratis</Link></a>
+                </div>
+            </div>
+           
+            <div className='image-index'></div>
+            
+
+        </div>
+    )
+}
+
+export default IndexContain;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,15 @@
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import IndexContain from "../../components/indexContain/IndexContain";
 
 function Home (){
     
     return (
         <div>
             <Header/>
+            <IndexContain/>
             <Footer/>
+            
         </div>
         
     );
